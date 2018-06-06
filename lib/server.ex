0137bb6cd8ac33d2ab431dcdd656dc318b693ed4
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-06-06 20:03:10 +0800

    Add in abstractions over GenServer callbacks

--- lib/server.ex
+++ lib/server.ex
@@ -1,7 +1,27 @@
 defmodule Sequence.Server do
   use GenServer
 
+  def init(args) do
+    {:ok, args}
+  end
+
   def handle_call(:next_number, _from, current_number) do
     {:reply, current_number, current_number + 1}
+  end
+
+  def handle_cast({:increment, delta}, state) do
+    {:noreply, state + delta}
+  end
+
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call(__MODULE__, :next_number)
+  end
+
+  def increment_number(delta) do
+    GenServer.cast(__MODULE__, {:increment, delta})
   end
 end
